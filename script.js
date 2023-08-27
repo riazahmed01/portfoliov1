258277bd47d4cae5c999d8d4e813f2c810d4f200
Riaz Ahmed <00.riaz.ahmed@gmail.com>
2023-08-27 01:03:09 -0400

    project and contact pages added

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-    // This code will run after the DOM has fully loaded
-
     const contactButton = document.querySelector('.contact');
-
-    // Add a click event listener to the contact button
-    contactButton.addEventListener('click', function() {
-        window.location.href = 'contact.html'; 
-    });
-    
-    // Get the GitHub button element
     const githubButton = document.querySelector('.github');
-    
-    // Add a click event listener to the GitHub button
-    githubButton.addEventListener('click', function() {
-        window.open('https://github.com/riazahmed01', '_blank'); 
-    });
-})
+    const projectButton = document.querySelector('.projectbtn');
+    const githubVisitButton = document.querySelector('.github-visit');
+
+    if (contactButton) {
+        contactButton.addEventListener('click', function() {
+            window.location.href = 'contact.html';
+        });
+    }
+
+    if (githubButton) {
+        githubButton.addEventListener('click', function() {
+            window.open('https://github.com/riazahmed01', '_blank');
+        });
+    }
+
+    if (projectButton) {
+        projectButton.addEventListener('click', function() {
+            window.open('https://github.com/riazahmed01', '_blank');
+        });
+    }
+
+    if (githubVisitButton) {
+        githubVisitButton.addEventListener('click', function() {
+            window.open('https://github.com/riazahmed01', '_blank');
+        });
+    }
+});
